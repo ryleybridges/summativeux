3f6c68954021146af1c2b498139e157f1a210c65
Ryley anne Bridges <932400076@bun2-206-12.local>
2019-04-08 11:21:56 +1200

    Added jquery function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-	
+	$(".fries-order").hide();
+
+	$(".btn-add").click(function(){
+		$(".fries-order").show();
+	});
+
+	$(".btn-remove").click(function(){
+		$(".fries-order").hide();
+	});
 })
